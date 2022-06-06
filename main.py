f97a72dad5bd22e32d5c8b43548370d0ac7bad0a
Ahmad Hafiizh Rachman <ahafiizhr@gmail.com>
2022-06-06 20:42:01 +0200

    Added argument header to define column name

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import pandas as pd
 import openpyxl
 
 
-def read_excel_sheet(spreadsheet: str, sheet: str):
-    sheet_dataframe = pd.read_excel(io=spreadsheet, sheet_name=sheet)
+def read_excel_sheet(spreadsheet: str, sheet: str, header: int = 0):
+    sheet_dataframe = pd.read_excel(io=spreadsheet, sheet_name=sheet, header=header)
     return sheet_dataframe
 
 
 if __name__ == "__main__":
-    start_module_dataframe = read_excel_sheet(spreadsheet="erp/erp_3.1.xlsx", sheet="Copy of ST")
+    start_module_dataframe = read_excel_sheet(spreadsheet="erp/erp_3.1.xlsx", sheet="Copy of ST", header=2)
 
     print(start_module_dataframe)
