0cd406483bf733434d0f77e77b4e45cf71c5d84c
Ahmad Hafiizh Rachman <ahafiizhr@gmail.com>
2022-06-07 12:59:30 +0200

    Added reshape dataframe, can select rows and columns

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ def read_excel_sheet(spreadsheet: str, sheet: str, header: int = 0):
     return sheet_dataframe
 
 
+def reshape_dataframe(dataframe: pd.DataFrame, rows: list = [], columns: list = []):
+    if not rows and not columns:
+        dataframe_adjusted = dataframe
+    elif not columns:
+        dataframe_adjusted = dataframe.iloc[rows[0]:rows[1], :]
+    elif not rows:
+        dataframe_adjusted = dataframe.iloc[:, columns[0]:columns[1]]
+    else:
+        dataframe_adjusted = dataframe.iloc[rows[0]:rows[1], columns[0]:columns[1]]
+
+    return dataframe_adjusted
+
+
 if __name__ == "__main__":
     start_module_dataframe = read_excel_sheet(spreadsheet="erp/erp_3.1.xlsx", sheet="Copy of ST", header=2)
-
+    start_module_dataframe_adjusted = reshape_dataframe(start_module_dataframe, columns=[1, 12])
     print(start_module_dataframe)
