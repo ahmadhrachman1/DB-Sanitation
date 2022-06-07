74f16c97aaa74525fcb5b6d843755044465f95fa
Ahmad Hafiizh Rachman <ahafiizhr@gmail.com>
2022-06-07 20:29:21 +0200

    Added change_drop_id_form for uniform drop id

--- main.py
+++ main.py
@@ -26,10 +26,22 @@ def reshape_dataframe(dataframe: pd.DataFrame, rows: list = [], columns: list = 
     return dataframe_adjusted
 
 
+def change_drop_id_form(dataframe: pd.DataFrame, column: str, fill_none: bool = False, to_datatype=None):
+    if fill_none:
+        dataframe[column] = dataframe[column].fillna(0)
+    if to_datatype:
+        dataframe[column] = dataframe[column].astype(to_datatype)
+    return dataframe
+
+
 if __name__ == "__main__":
     start_module_dataframe = read_excel_sheet(spreadsheet="erp/erp_3.1.xlsx", sheet="Copy of ST", header=2)
     start_module_dataframe_reshaped = reshape_dataframe(start_module_dataframe, columns=[1, 12], drop_rows=[0],
-                                                        drop_columns=["Start Module Pre-Production Status",
+                                                        drop_columns=["Hardware Version & Type",
+                                                                      "Start Module Pre-Production Status",
                                                                       "Assigned to Customer ID",
                                                                       "Assigned to Order ID"])
+    start_module_dataframe_reshaped = change_drop_id_form(start_module_dataframe_reshaped,
+                                                          column="Drop App Code",
+                                                          fill_none=True, to_datatype=int)
     print(start_module_dataframe_reshaped)
